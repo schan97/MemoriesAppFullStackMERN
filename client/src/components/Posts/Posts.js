5c5c311001e61e684fb8f54aeebdcb40c62a39a5
Steven Chan <you@example.com>
2022-02-28 16:21:42 -0500

    Implement Posts. Fix Bug with postsController. Re-add posts actions.

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,17 +2,29 @@ import React from 'react';
 import Post from './Post/Post.js'
 import useStyles from './styles'
 import { useSelector } from 'react-redux';
+import {Grid, CircularProgress} from '@material-ui/core';
     
 const Posts = () => {
     const posts = useSelector((state) => state.posts);
     const classes = useStyles();
+
+    console.log(posts);
+
     return(
-        <>
-        <h1>Posts</h1>
-        <Post/>
-        <Post/>
-        </>
-        
+        // checks if posts is populated, if not show loading circle, 
+        // else show the Grid with each post (looped with map)
+        !posts.length ? <CircularProgress/> : (
+            <Grid className={classes.mainContainer} container alignItems='stretch' spacing={3}>
+                {
+                    posts.map((post) => (
+                        <Grid key={post._id} item xs={12} sm={6}>
+                            <Post post={post}/>
+                        </Grid>
+
+                    ))
+                }
+            </Grid>
+        )
     );
 }
 
