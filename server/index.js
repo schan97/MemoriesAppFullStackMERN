38f03eee1e4ebb0f7aa2bad5532cf767d2b8f9ca
Steven Chan <you@example.com>
2022-03-07 14:33:51 -0500

    Add environmental variables

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js'
 
 const app = express();
+dotenv.config();
+
 // File Size Limit
 app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
@@ -14,13 +16,9 @@ app.use(cors());
 app.use('/posts', postRoutes)
 
 
-
-
-
-const CONNECTION_STRING = "mongodb+srv://memories-user:memories-user123@cluster0.lyiad.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
