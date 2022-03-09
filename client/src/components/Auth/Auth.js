1011fa72b841673fd2adf8922fbc9d433cb83b60
Steven Chan <you@example.com>
2022-03-09 17:10:34 -0500

    Implement Google Sign In and Logout. Added auth reducer

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -3,14 +3,19 @@ import {Avatar, Button, Paper, Grid, Typography, Container, TextField} from '@ma
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import useStyles from './styles';
 import Input from './Input';
-
+import {GoogleLogin} from 'react-google-login';
+import Icon from './Icon';
+import {useDispatch} from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const Auth = () => {
   const classes = useStyles();
-
   const [showPassword, setShowPassword] = useState(false);
-
   const [isSignUp, setIsSignUp] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+
 
   const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
 
@@ -26,6 +31,29 @@ const Auth = () => {
     setIsSignUp((prevIsSignUp) => !prevIsSignUp);
     setShowPassword(false);
   };
+
+  const googleSuccess = async (res) => {
+    //console.log(res);
+    const result = res?.profileObj;
+    const token = res?.tokenId;
+
+    try{
+      dispatch({type: 'AUTH', data: {result, token}})
+
+      // navigate to home after google login success
+      navigate("/");
+    }
+    catch(error)
+    {
+      console.log(error);
+    }
+  };
+
+  const googleFailure = (error) => {
+    console.log(error);
+    console.log("Google Sign In was unsuccessful. Try Again Later");
+  };
+
 
   return (
     <>
@@ -66,6 +94,17 @@ const Auth = () => {
                   )
               }
             </Button>
+            <GoogleLogin
+              clientId='283669330184-mn1cdf8pfjbob5tl0cn0tu6vk27095a3.apps.googleusercontent.com'
+              render={(renderProps) => (
+                <Button className={classes.googleButton} color='primary' fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon/>} variant='contained'>
+                    Google Sign In
+                  </Button>
+              )}
+              onSuccess={googleSuccess}
+              onFailure={googleFailure}
+              cookiePolicy="single_host_origin"
+            />
 
             <Grid container justifyContent="flex-end">
               <Grid item>
