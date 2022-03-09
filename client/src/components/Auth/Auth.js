50bcade942b53a68251de76ae4218da86131cca8
Steven Chan <you@example.com>
2022-03-09 16:09:43 -0500

    Fleshed out Auth Component and Created Input Component

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,8 +1,89 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Avatar, Button, Paper, Grid, Typography, Container, TextField} from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import useStyles from './styles';
+import Input from './Input';
+
 
 const Auth = () => {
+  const classes = useStyles();
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
+
+  const handleSubmit = () => {
+
+  };
+
+  const handleChange = () => {
+
+  };
+
+  const switchMode = () => {
+    setIsSignUp((prevIsSignUp) => !prevIsSignUp);
+    setShowPassword(false);
+  };
+
   return (
-    <div>Auth</div>
+    <>
+      <Container component="main" maxWidth="xs">
+        <Paper className={classes.paper} elevation={3}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon/>
+          </Avatar>
+          {/* Check what to display based on bool */}
+          <Typography variant='h5'>{isSignUp ? 'Sign Up' : 'Sign In'}</Typography>
+          <form className={classes.form} onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+              {
+                // if isSignUp == true
+                // && is used for ternery operators with a second value being null
+                isSignUp && (
+                  <>
+                    <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half/>
+                    <Input name="lastName" label="Last Name" handleChange={handleChange} autoFocus half/>
+                  </>
+                ) 
+              }
+              <Input name="email" label="Email Address" handleChange={handleChange} type="email"/>
+              <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword}/>
+              {
+                isSignUp && (
+                  <Input name="confirmPassword" label="Confirm Password" handleChange={handleChange} type="password"/>
+                )
+              }
+            </Grid>
+            <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}>
+              {/* Show sign up text if the user is signing up else show sign in */}
+              {
+                  isSignUp ? (
+                    'Sign Up'
+                  ) : (
+                    'Sign In'
+                  )
+              }
+            </Button>
+
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Button onClick={switchMode}>
+                  {
+                    isSignUp ? (
+                      "Already have an account? Sign In"
+                    ) : (
+                      "Don't have an account? Sign Up"
+                    )
+                  }
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </Paper>
+      </Container>
+    </>
   )
 }
 
--- client/src/components/Form/styles.js
+++ client/src/components/Form/styles.js
@@ -13,6 +13,7 @@ export default makeStyles((theme) => ({
     display: 'flex',
     flexWrap: 'wrap',
     justifyContent: 'center',
+    padding: theme.spacing(2),
   },
   fileInput: {
     width: '97%',
