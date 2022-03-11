6523dd75e71992074d924d6e672335da260072b0
Steven Chan <you@example.com>
2022-03-11 16:02:01 -0500

    Flesh out the signin and signup function calls in auth.js actions. Refactor api index.js

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,10 +1,12 @@
 import * as api from '../api/index.js';
 import { AUTH } from '../constants/actionTypes.js';
 
-export const signin = () => async (dispatch,navigate) => {
+export const signin = (formData,navigate) => async (dispatch) => {
     try{
         // login the user
+        const {data} = await api.signIn(formData);
 
+        dispatch({type: AUTH, data});
         navigate("/");
     }
     catch (error) {
@@ -12,9 +14,12 @@ export const signin = () => async (dispatch,navigate) => {
     }
 }
 
-export const signup = () => async (dispatch,navigate) => {
+export const signup = (formData,navigate) => async (dispatch) => {
     try{
         // login the user
+        const {data} = await api.signUp(formData);
+
+        dispatch({type: AUTH, data});
 
         navigate("/");
     }
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,14 +2,15 @@ import axios from 'axios';
 
 // url pointing to our backend route that we created and specified
 // const url = 'https://app-memories-proj.herokuapp.com/posts'
-const url = 'http://localhost:5000/posts'
+// const url = 'http://localhost:5000/posts'
 
-export const fetchPosts = () => axios.get(url);
+const API = axios.create({baseURL: 'http://localhost:5000'})
 
-export const createPost = (newPost) => axios.post(url, newPost);
+export const fetchPosts = () => axios.get('/posts');
+export const createPost = (newPost) => axios.post('/posts', newPost);
+export const updatePost = (id, updatedPost) => axios.patch(`'/posts'/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`'/posts'/${id}`);
+export const likePost = (id) => axios.patch(`'/posts'/${id}/likePost`);
 
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
-
-export const deletePost = (id) => axios.delete(`${url}/${id}`);
-
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+export const signIn = (formData) => API.post('/user/signin',formData);
+export const signUp = (formData) => API.post('/user/signup',formData);
