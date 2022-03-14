7a121e56e4c836862c330eb85b35bc4f7ca5f0c5
Steven Chan <you@example.com>
2022-03-14 13:05:22 -0400

    Changed post component to display number of likes, Hide edit and delete buttons from users that didn't create the post.

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import useStyles from './styles'
 import {Card, CardActions, CardContent, CardMedia, Button, Typography} from '@material-ui/core';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import ThumbUpAltOutlined from '@material-ui/icons/ThumbUpAltOutlined';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import {useDispatch} from 'react-redux';
@@ -13,8 +14,32 @@ import moment from 'moment';
 const Post = ({post, setCurrentId}) => {
     const classes = useStyles();
     const dispatch = useDispatch();
+    const user = JSON.parse(localStorage.getItem('profile'));
+
+    // Likes Sub component
+    const Likes = () => {
+        if (post.likes.length > 0)
+        {
+            // loop through the array of likes and check if the currently
+            // signed in userId is contained inside the likes array
+            return post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id))
+                ? (
+                    // If it's in the likes array, the user already liked the post and display:
+                    <>
+                        <ThumbUpAltIcon fontSize="small"/>&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}`}
+                    </>
+                ) : (
+                    // else user hasn't liked the post and display:
+                    <>
+                        <ThumbUpAltOutlined fontSize="small"/>&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}
+                    </>
+                );
+        }
+        // return the default like button if no user liked the post
+        return <><ThumbUpAltOutlined fontSize="small"/>&nbsp;Like</>
 
 
+    }
 
     return(
         <Card className={classes.card}>
@@ -26,12 +51,16 @@ const Post = ({post, setCurrentId}) => {
                 <Typography variant='body2'>{moment(post.createdAt).fromNow()}</Typography>
             </div>
 
-            {/* Ellipse Icon */}
-            <div className={classes.overlay2}>
-                <Button style={{color: "white"}} size="small" onClick={() => setCurrentId(post._id)}>
-                    <MoreHorizIcon fontSize="medium"/>
-                </Button>
-            </div>
+            {/* Only the creator of the post can edit it */}
+            {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+                // Edit Ellipse
+                <div className={classes.overlay2}>
+                    <Button style={{color: "white"}} size="small" onClick={() => setCurrentId(post._id)}>
+                        <MoreHorizIcon fontSize="medium"/>
+                    </Button>
+                </div>
+            )}
+            
 
             {/* Details / Tags */}
             <div className={classes.details}>
@@ -48,17 +77,23 @@ const Post = ({post, setCurrentId}) => {
             <CardActions className={classes.cardActions}>
 
                 {/* Like */}
-                <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
-                    <ThumbUpAltIcon fontSize="small"/>
+                <Button size="small" disabled={!user?.result} color="primary" onClick={() => dispatch(likePost(post._id))}>
+                    {/* <ThumbUpAltIcon fontSize="small"/>
                     &nbsp; Like &nbsp;
-                    {post.likeCount}
+                    {post.likeCount} */}
+                    <Likes/>
                 </Button>
 
-                {/* Delete */}
-                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
-                    <DeleteIcon fontSize="small"/>
-                    Delete
-                </Button>
+                {/* Only the creator of the post can delete it */}
+                {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+                    /* Delete */
+                    <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
+                        <DeleteIcon fontSize="small"/>
+                        Delete
+                    </Button>
+                )}
+
+                
             </CardActions>
 
         </Card>
