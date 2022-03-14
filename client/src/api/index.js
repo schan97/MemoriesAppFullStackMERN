4385f35d6f9d6131e590810c03fb6ab9df82747a
Steven Chan <you@example.com>
2022-03-14 17:06:28 -0400

    Change api url to hosted url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // const url = 'https://app-memories-proj.herokuapp.com/posts'
 // const url = 'http://localhost:5000/posts'
 
-const API = axios.create({baseURL: 'http://localhost:5000'});
+const API = axios.create({baseURL: 'https://app-memories-proj.herokuapp.com/posts'});
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
