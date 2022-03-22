79648eb5ec10c47260cede5adfb3bacc0428dbca
Steven Chan <you@example.com>
2022-03-22 16:38:46 -0400

    Fetched the comments and displayed them. Added auto scrolling after commenting

--- client/src/components/PostDetails/CommentSection.jsx
+++ client/src/components/PostDetails/CommentSection.jsx
@@ -9,14 +9,26 @@ import { mergeClasses } from '@material-ui/styles';
 const CommentSection = ({post}) => {
     const classes = useStyles();
     const dispatch = useDispatch();
-    const [comments, setComments] = useState([1,2,3,4]);
+    const commentsRef = useRef();
+    const [comments, setComments] = useState(post?.comments);
     const [comment, setComment] = useState("");
     const user = JSON.parse(localStorage.getItem('profile'));
 
-    const handleClick = () => {
+    const handleClick = async () => {
         const finalComment = `${user.result.name}: ${comment}`
 
-        dispatch(commentPost(finalComment, post._id));
+        // gets the response of the latest comments array
+        const newComments = await dispatch(commentPost(`${user?.result?.name}: ${comment}`, post._id));
+
+        // update the comments after posting
+        setComments(newComments);
+
+        // clears the comment input field
+        setComment("");
+
+        // scrolls to the anchor/ref point we declared
+        commentsRef.current.scrollIntoView({behavior: 'smooth'});
+
     };
 
     return(
@@ -26,9 +38,14 @@ const CommentSection = ({post}) => {
                     <Typography gutterBottom variant='h6'>Comments</Typography>
                     {comments.map((comment, index) => (
                         <Typography key={index} gutterBottom variant='subtitle1'>
-                            Comment {index}
+                            {/* Bold the name */}
+                            <strong>{comment.split(':')[0]}</strong>
+                            :
+                            {/* Actual comment */}
+                            {comment.split(':')[1]}
                         </Typography>
                     ))}
+                    <div ref={commentsRef}/>
                 </div>
 
                 {user?.result?.name && (
